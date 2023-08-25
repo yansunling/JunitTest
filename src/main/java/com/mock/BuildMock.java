6172366cf1f85d9923c1c223143f87f11da86fcd
yansunling <yansunling@126.com>
2023-08-25 08:29:54 +0800

    生成mock数据

--- src/main/java/com/mock/BuildMock.java
+++ src/main/java/com/mock/BuildMock.java
@@ -4,9 +4,25 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.jsonzou.jmockdata.JMockData;
 import com.mock.data.CRMX_relation_visitCust_visitVO;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+
 public class BuildMock {
     public static void main(String[] args) {
-        CRMX_relation_visitCust_visitVO mock = JMockData.mock(CRMX_relation_visitCust_visitVO.class);
-        System.out.println(JSONObject.toJSONString(mock));
+//        CRMX_relation_visitCust_visitVO mock = JMockData.mock(CRMX_relation_visitCust_visitVO.class);
+//        System.out.println(JSONObject.toJSONString(mock));
+
+
+        List<String> allPerfectCustomer = Arrays.asList("1","2","3","4","5","6");
+
+
+
+        allPerfectCustomer.stream().forEach(item->{
+            System.out.println(item);
+        });
+
+
     }
 }
